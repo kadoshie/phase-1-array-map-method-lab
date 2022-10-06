095f51644cca971334aa7f4b333c3d83d5773976
Moringa School <ruthmutiso69@gmail.com>
2022-10-06 10:55:55 +0300

    completed

--- index.js
+++ index.js
@@ -1,16 +1,23 @@
 const tutorials = [
-  'what does the this keyword mean?',
-  'What is the Constructor OO pattern?',
-  'implementing Blockchain Web API',
-  'The Test Driven Development Workflow',
-  'What is NaN and how Can we Check for it',
-  'What is the difference between stopPropagation and preventDefault?',
-  'Immutable State and Pure Functions',
-  'what is the difference between == and ===?',
-  'what is the difference between event capturing and bubbling?',
-  'what is JSONP?'
+  "What Does The This Keyword Mean?",
+  "What Is The Constructor OO Pattern?",
+  'Implementing Blockchain Web API',
+  "The Test Driven Development Workflow",
+  "What Is NaN and How Can We Check For It",
+  'What Is The Difference Between StopPropagation And PreventDefault?',
+  'Immutable State And Pure Functions',
+  'What Is The Difference Between == And ===?',
+  'What Is The Difference Between Event Capturing And Bubbling?',
+  'What Is JSONP?'
 ];
 
 const titleCased = () => {
-  return tutorials
+  return tutorials.map((globNames) => {
+    const splitExpln = globNames.split(' ');
+    return splitExpln.map((exp) => {
+      return exp.charAt(0).toUpperCase() + exp.slice(1);
+    }).join(' ');
+  })
 }
+  
+
